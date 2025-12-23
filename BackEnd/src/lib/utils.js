2864c42f33218a5857fd4bebf4e2a5af99f2563c
yash <ysiddhapura6@gmail.com>
2025-12-23 14:29:53 +0530

    change utilities

--- BackEnd/src/lib/utils.js
+++ BackEnd/src/lib/utils.js
@@ -9,7 +9,7 @@ export const tokenGenerator = (userId , res) => {
         maxAge : 7 * 24 * 60 * 60 * 1000,
         httpOnly : true,
         sameSite:"strict",
-        secure:'development'
+        secure: process.env.NODE_ENV === 'production'
     })
     
     return token
